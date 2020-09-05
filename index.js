dab907077a9d54fe39aa5902b538a0bb05c18e1f
Kevin Audibert <kevin.audibert26@gmail.com>
2020-09-05 22:52:45 +0200

    function affiche nom et prix nounours dans un paragraphe html de la div nounours

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 //const fetch = require("node-fetch");
 
-async function retrieveData(url) {
-    let response = await fetch(url);
+    fetch("http://localhost:3000/api/teddies")
+    .then(response => response.json())
+    .then(nounourses => listeProductNounours(nounourses));
 
-    if (response.ok) {
-        let json = await response.json();
-        console.log(json);
-    } else {
-        alert("HTTP-Error: " + response.status);
-    }
-
-}
-
-function addParagraphe(txt) {
-    let div = document.getElementById('test')
+function addParagraphe(txt, price, description) {
+    let div = document.getElementById('nounours')
     let para = document.createElement("p")
-    let contenu = document.createTextNode(txt)
-    div.appendChild(para)
+    let contenu = document.createTextNode(`${txt} coûte ${price/100} € ${description}`)
     para.appendChild(contenu)
+    div.appendChild(para)
 }
 
-retrieveData("http://localhost:3000/api/teddies")
+function listeProductNounours(nounourses) {
+    for (let nounours of nounourses) {
+        addParagraphe(nounours.name, nounours.price, nounours.description)
+    }
+}
