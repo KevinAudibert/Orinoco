23ae859820cf825734dcf367a466eff71a030e46
Kevin Audibert <kevin.audibert26@gmail.com>
2020-11-02 18:56:09 +0100

    rajout prix et photo

--- js/panier.js
+++ js/panier.js
@@ -6,23 +6,108 @@
 //    })
 //}
 
+async function load(id) {
+    let response = await fetch(`http://localhost:3000/api/teddies/${id}`);
+    let data = await response.json();
+    let tableau = document.getElementsByClassName(`image_${id}`)
+        for (let image of tableau) {
+            image.src = data.imageUrl
+        }
+        let tab = document.getElementsByClassName(`price_${id}`)
+        for (let price of tab) {
+            price.textContent = `Prix unitaire : ${data.price/100} €`
+        }
+}
+
+function priceTotal() {
+    
+}
+
+//Fonction qui permet de faire le recapitulatif des produit stockés dans le localStorage
 function createCart() {
+
     //Zone du panier
     let panier = document.getElementById('content_table')
-    
+        
     //boucle qui reccup les keys du Panier
     for (let keys of Object.keys(localStorage)) {
 
         //On reprend tous les couleurs commandées d'un produit 
         for (let product of JSON.parse(localStorage[keys])) {
+
+            //variables
             let produit = document.createElement("article")
-            let contenu = document.createTextNode(`la Peluche ${product.name} de couleur ${product.color} a été commandé ${product.quantity} fois`)
+            let div = document.createElement('div')
+            
+            //Ajout au DOM
             panier.appendChild(produit)
-            produit.appendChild(contenu)
+            addImageTeddy(produit, product.id)
+            
+            produit.appendChild(div)
+            addNameTeddy(div, product.name)
+            addColorTeddy(div, product.color)
+            addQuantityTeddy(div, product.quantity)
+            addPriceTeddy(div, product.id)
+
+            //Ajout du bouton pour supprimer l'article du localStorage
+            btnRemoveItems(produit, keys)
         }
+        load(keys)
     }
 }
 
+function addImageTeddy(produit, id) {
+    let image = document.createElement('img')
+    image.className = `image_${id}`
+    produit.appendChild(image)    
+}
+
+function addPriceTeddy(produit, id) {
+    let price = document.createElement('p')
+    price.className = `price_${id}`
+    produit.appendChild(price)
+}
+
+//fonction permettant de créer un bouton pour supprimer un article
+function btnRemoveItems(parent, produit) {
+    let btnRemove = document.createElement('button');
+    let contents = document.createTextNode('Supprimer');
+    btnRemove.type='submit'
+    btnRemove.addEventListener('click', emptyCart)
+    parent.appendChild(btnRemove);
+    btnRemove.appendChild(contents);
+}
+
+//fonction permettant de créer balise pour le nom du Teddy
+function addNameTeddy(parent, txt) {
+    let newPara = document.createElement('h4');
+    let contenu = document.createTextNode(txt);
+    parent.appendChild(newPara);
+    newPara.appendChild(contenu);
+}
+
+//fonction permettant de créer balise pour la couleur 
+function addColorTeddy(parent, color) {
+    let newPara = document.createElement('p');
+    let contenu = document.createTextNode(`Couleur : ${color}`);
+    parent.appendChild(newPara);
+    newPara.appendChild(contenu);
+}
+
+//fonction permettant de créer balise pour la quantitée
+function addQuantityTeddy(parent, quantity) {
+    let newPara = document.createElement('p');
+    let contenu = document.createTextNode(`Quantité : ${quantity}`);
+    parent.appendChild(newPara);
+    newPara.appendChild(contenu);
+}
+
+//fonction permettant de supprimer l'article
+function emptyCart(produit) {
+    window.localStorage.removeItem(produit); 
+ }
+
+//Fonction permettant de mettre en forme de formulaire rempli par l'utilisateur pour l'envoi vers API 
 function createFormSend() {
 
     //variables qui reccupere les valeur du formulaire 
@@ -39,17 +124,7 @@ function createFormSend() {
         address : addressForm,
         city : cityForm,
     }
-    let products = []
+    let products = Object.keys(localStorage)
     let send = { contact, products }
     console.log(JSON.stringify(send))
-}
-
-function btnSendOrder() {
-    let div = document.getElementById('form')
-    let contenu = document.createTextNode('Envoyer')
-    let btnSendForm = document.createElement('button');
-    btnSendForm.type='submit'
-    btnSendForm.addEventListener('click', createFormSend())
-    div.appendChild(btnSendForm);
-    btnSendForm.appendChild(contenu);
 }
